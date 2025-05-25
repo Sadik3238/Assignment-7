cf1cf34cce45dfbfaeb8eef877bb706a9644205f
Mihun-Codex <mithunislaam390@gmail.com>
2025-05-25 22:50:01 +0600

    added replacement

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base :'/Assignment-7/',
+  plugins: [react(),tailwindcss(),],
 })
 
-
-// // vite.config.js
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/Assignment-7/', // Replace with your repo name
-// });
