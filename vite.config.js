c5afb8c92032399b4810fc7af8580e37310bab3f
Mihun-Codex <mithunislaam390@gmail.com>
2025-05-25 23:48:31 +0600

    add change some

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
-  base: '/simple-react-assignment-7/',
+  base: '/Assignment-7/',
 })
 
